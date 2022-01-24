6db839506be36985079bb6c8bae9c528a73da3aa
Mario <koddenbrock@gmail.com>
2022-01-24 20:17:28 +0100

    added waiting seconds

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+# %% this comment is just for my vs code ide
+
 from pydoc import HTMLRepr
 from htmldownloader import HtmlDownloader
 
 
-html = HtmlDownloader()
+html = HtmlDownloader(waiting_seconds=600)
 html.run()
+
+# %%
